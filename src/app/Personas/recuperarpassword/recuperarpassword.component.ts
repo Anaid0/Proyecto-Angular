7f4c5bf23f480ab2cc58e7dae0baafd78e81bd5a
Anaid0 <vpdiana06@gmail.com>
2024-11-18 13:53:41 -0500

    init

---
+++ src/app/Personas/recuperarpassword/recuperarpassword.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recuperarpassword',
+  standalone: true,
+  imports: [],
+  templateUrl: './recuperarpassword.component.html',
+  styleUrl: './recuperarpassword.component.css'
+})
+export class RecuperarpasswordComponent {
+
+}
---
+++ src/app/Personas/resgistrar/resgistrar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resgistrar',
+  standalone: true,
+  imports: [],
+  templateUrl: './resgistrar.component.html',
+  styleUrl: './resgistrar.component.css'
+})
+export class ResgistrarComponent {
+
+}
